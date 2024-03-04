39c6ffabd43a4cd74eea9467413b43e20b6ec6ca
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-03-04 23:29:44 +0600

    updated navigation scroll top - mobile browser issue fix

--- scripts/main.js
+++ scripts/main.js
@@ -22,11 +22,36 @@
 
   /**
    * Navbar effects and scrolltop buttons upon scrolling
-   */
+   
   const navbar = document.getElementById("header-nav");
   var body = document.getElementsByTagName("body")[0];
   const scrollTop = document.getElementById("scrolltop");
   window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add("fixed-top", "shadow-sm");
+      body.style.paddingTop = navbar.offsetHeight + "px";
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove("fixed-top", "shadow-sm");
+      body.style.paddingTop = "0px";
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+*/
+  const navbar = document.getElementById("header-nav");
+  var body = document.getElementsByTagName("body")[0];
+  const scrollTop = document.getElementById("scrolltop");
+  window.onscroll = () => {
+    if (window.innerWidth < 992) {
+      navbar.classList.add("fixed-top", "shadow-sm");
+      body.style.paddingTop = navbar.offsetHeight + "px";
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+      return;
+    } // exit if width less than 1200px
+
     if (window.scrollY > 0) {
       navbar.classList.add("fixed-top", "shadow-sm");
       body.style.paddingTop = navbar.offsetHeight + "px";
@@ -90,7 +115,6 @@
       });
     });
   });
-
 
   // Auto writing function
   function autoWrite(element, text, speed) {
