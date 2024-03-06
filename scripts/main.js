4a9638b62730f09066dfc17f224b0fe7bf11c6bb
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-03-07 01:32:22 +0600

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -71,58 +71,6 @@
     }
   };
 
-  /**
-   * Masonry Grid
-   
-  var elem = document.querySelector(".grid");
-  if (elem) {
-    imagesLoaded(elem, function () {
-      new Masonry(elem, {
-        itemSelector: ".grid-item",
-        percentPosition: true,
-        horizontalOrder: true,
-      });
-    });
-  }
-*/
-  /**
-   * Big Picture Popup for images and videos
-   
-  document.querySelectorAll("[data-bigpicture]").forEach(function (e) {
-    e.addEventListener("click", function (t) {
-      t.preventDefault();
-      const data = JSON.parse(e.dataset.bigpicture);
-      BigPicture({
-        el: t.target,
-        ...data,
-      });
-    });
-  });
-*/
-  /**
-   * Big Picture Popup for Photo Gallary
-   
-  document.querySelectorAll(".bp-gallery a").forEach(function (e) {
-    var caption = e.querySelector("figcaption");
-    var img = e.querySelector("img");
-    // set the link present on the item to the caption in full view
-    img.dataset.caption =
-      '<a class="link-light" target="_blank" href="' +
-      e.href +
-      '">' +
-      caption.innerHTML +
-      "</a>";
-    window.console.log(caption, img);
-    e.addEventListener("click", function (t) {
-      t.preventDefault();
-      BigPicture({
-        el: t.target,
-        gallery: ".bp-gallery",
-      });
-    });
-  });
-*/
-
   // Auto writing function
   function autoWrite(element, text, speed) {
     let i = 0;
